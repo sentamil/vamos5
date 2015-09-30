8ecb60cd48246380ba4e8ff447dabd24a8b6c36b
Prasanna K <prkothan@gmail.com>
2015-09-30 06:32:44 +0000

    remind.blade.php edited online with Bitbucket

--- app/views/password/remind.blade.php
+++ app/views/password/remind.blade.php
@@ -3,12 +3,15 @@
 @elseif (Session::has('success'))
   An email with the password reset has been sent.
 @endif
- 
-{{ Form::open(array('route' => 'password.request')) }}
- 
-  <p>{{ Form::label('userId', 'userId') }}
-  {{ Form::text('userId') }}</p>
- 
+
+
+
+{{ Form::open(array('route' => 'password.postremind')) }}
+
+  <p>{{ Form::label('userid', 'userid') }}
+  {{ Form::text('userid') }}</p>
+
+
   <p>{{ Form::submit('Send Reminder') }}</p>
- 
+
 {{ Form::close() }}
