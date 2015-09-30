35bd8167d51d90c375e02fde2804a9ea5cf36d4c
prkothan <prkothan@gmail.com>
2015-09-30 12:03:22 +0530

    passwd reset issue -- corrected again

--- app/views/vdm/users/edit.blade.php
+++ app/views/vdm/users/edit.blade.php
@@ -1,66 +1,48 @@
-<html>
-<body>
 @extends('includes.vdmheader')
 @section('mainContent')
-<div id="wrapper">
-  <div class="content animate-panel">
-     <div class="row">
-       <div class="col-lg-12">
-	 <div class="hpanel">
-		<div class="panel-heading">
-                     <h1>Edit User</h1>
-		<div class="panel-body">
-		<div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer"><div class="row">
-		<div claa="row">
-		<div class="col-sm-12">
 
-                       <!-- if there are creation errors, they will show here -->
-			{{ HTML::ul($errors->all()) }}
+<h1>Edit User</h1>
+
+<!-- if there are creation errors, they will show here -->
+{{ HTML::ul($errors->all()) }}
 
 
-			{{ Form::model($userId, array('route' => array('vdmUsers.update', $userId), 'method' => 'PUT')) }}
+{{ Form::model($userId, array('route' => array('vdmUsers.update', $userId), 'method' => 'PUT')) }}
 
-			<div class="form-group">
-			{{ Form::label('userId', 'UserId :') }}
 
-		       {{ Form::label('userId', $userId) }}
-   			</div><br>
-			</br>
-			<div class="form-group">
-			   {{ Form::label('MobileNo', 'MobileNo') }}
-			   {{ Form::text('MobileNo', $mobileNo, array('class' => 'form-control')) }}
-			</div><br><br>
-			<div class="form-group">
-			{{ Form::label('Email', 'Email') }}
-			{{ Form::text('Email', $email, array('class' => 'form-control')) }}
-			</div>
-			<div style="text-align: right;"><br>
-			{{ Form::submit('Update the User!', array('class' => 'btn btn-primary')) }}
-			</div>
-			<hr>
-			<table id="example1" class="table table-bordered dataTable">
-			<thead>
-			<tr>
-	                      <th>{{ Form::label('vehicleList', 'Select the groups:') }}</th>
-			</tr>
-		</thead>
-		<tbody>
-	        @if(isset($vehicleGroups))
+	<div class="form-group">
+		{{ Form::label('userId', 'UserId :') }}
+		<br/>
+		{{ Form::label('userId', $userId) }}
+	</div>
+
+<div class="form-group">
+		{{ Form::label('mobileNo', 'mobileNo') }}
+		{{ Form::text('mobileNo', $mobileNo, array('class' => 'form-control')) }}
+	</div>
+<div class="form-group">
+		{{ Form::label('email', 'email') }}
+		{{ Form::text('email', $email, array('class' => 'form-control')) }}
+	</div>
+
+		 <div class="form-group">
+	 {{ Form::label('vehicleList', 'Select the groups:') }}
+	 </div>
+	  @if(isset($vehicleGroups))
 		@foreach($vehicleGroups as $key => $value)
-		<tr class="col-md-2">
-		<td>	{{ Form::checkbox('vehicleGroups[]', $key,  in_array($value,$selectedGroups), ['class' => 'field']) }}
+			 
+			{{ Form::checkbox('vehicleGroups[]', $key,  in_array($value,$selectedGroups), ['class' => 'field']) }}
 			{{ Form::label($value) }}
-
-		</td>
-		</tr>
+			<br/>
 		@endforeach
-		</tbody>
-		</table>
 		@endif
+		</br/>
+		</br/>
+		 
+        
+        </br/>
+        </br/>
+	
+	{{ Form::submit('Update the User!', array('class' => 'btn btn-primary')) }}
+
 {{ Form::close() }}
-</div>
-</div>
-</div>
-@stop
-</body>
-</html>
