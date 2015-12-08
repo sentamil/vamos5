ee94febbad03fe7651ecdef46f6c6434dbb848d7
Prasanna <prkothan@gmail.com>
2015-12-08 09:58:06 +0530

    no message

--- app/views/vdm/business/create.blade.php
+++ app/views/vdm/business/create.blade.php
@@ -9,9 +9,6 @@
                    		 <h4><b><font color="blue">ADD DEVICE</font></b></h4>
                 	 </div>
                 	<div class="panel-body">
-					Available licence :  {{Session::get('availableLincence')}}
-					</br>
-					
                 		<div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer">
                 			<div class="row">
                 				<div class="col-sm-12">
@@ -21,13 +18,11 @@
 										<div class="col-md-12">
 											<div class="col-md-6">
 												<div class="row">
-
-													<div class="col-md-6">
-														{{ Form::label('numberofdevice', 'Number of device') }}
-
+													<div class="col-md-3">
+														<h5>{{ Form::label('deviceId', 'Device ID') }}</h5>
 													</div>
 													<div class="col-md-6">
-														{{ Form::text('numberofdevice', Input::old('numberofdevice'), array('class' => 'form-control')) }}
+														{{ Form::textarea('deviceId', Input::old('deviceId'), array('class' => 'form-control')) }}
 													</div>
 												</div>
 											</div>
