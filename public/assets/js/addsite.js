b117db7234dc27ba40f48236755f9f74031e94fc
arun_muhesh <mailtoarunmuhesh@vamosystems.in>
2016-03-03 19:42:50 +0530

    changes

--- public/assets/js/addsite.js
+++ public/assets/js/addsite.js
@@ -1,33 +1,4 @@
 var getIP = globalIP;
-app.directive('map', function($http, vamoservice) {
-    return {
-        restrict: 'E',
-        replace: true,
-        template: '<div></div>',
-        link: function(scope, element, attrs) {
-       //     vamoservice.getDataCall(scope.url).then(function(data) {
-		   		// var locs = data;
-
-		  //  		var myOptions = {
-				// 	zoom: 13,
-				// 	center: new google.maps.LatLng(12.993803, 80.193075),
-				// 	mapTypeId: google.maps.MapTypeId.ROADMAP
-				// 	//styles: [{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]}]
-    //         	};
-    //         	scope.map = new google.maps.Map(document.getElementById(attrs.id), myOptions);
-				// google.maps.event.addListener(scope.map, 'click', function(event) {
-				// 				scope.clickedLatlng = event.latLng.lat() +','+ event.latLng.lng();
-				// 				// $('#latinput').val(scope.clickedLatlng);
-				// 			});
-			// });
-		  // $(document).on('pageshow', '#maploc', function(e, data){       
-    //             	google.maps.event.trigger(document.getElementById('	maploc'), "resize");
-    //        		 });
-		  	
-		  
-        }
-    };
-});
 
 app.controller('mainCtrl',function($scope, $http){
 
