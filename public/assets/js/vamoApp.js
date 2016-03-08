afc8d302ec5f51190cb25e018f84940c48630f60
arun_muhesh <mailtoarunmuhesh@vamosystems.in>
2016-03-08 16:25:00 +0530

    chnages

--- public/assets/js/vamoApp.js
+++ public/assets/js/vamoApp.js
@@ -10,12 +10,12 @@ var logo =document.location.host;
 
 function ValidateIPaddress(ipaddress)   
 {  
-var ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;  
-if(ipaddress.match(ipformat)) {
-  return (true)  
-}  
-// alert("You have entered an invalid IP address!")  
-return (false)  
+	var ipformat = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;  
+	if(ipaddress.match(ipformat)) {
+	  return (true)  
+	}  
+	// alert("You have entered an invalid IP address!")  
+	return (false)  
 }  
 
 
@@ -28,9 +28,13 @@ if(ValidateIPaddress(logo)) {
 var imgName= '/vamo/public/assets/imgs/'+logo+'.small.png';
 
 $('#imagesrc').attr('src', imgName);
-// var gmarkers=[];
-// var ginfowindow=[];
-// var geomarker=[];
-// var geoinfo=[];
+var gmarkers=[];
+var ginfowindow=[];
+var geomarker=[];
+var geoinfo=[];
 var app = angular.module('mapApp',['ui.bootstrap']);
 
+app.run(function($rootScope) {
+    $rootScope.test = new Date();
+    console.log($rootScope.test)
+});
