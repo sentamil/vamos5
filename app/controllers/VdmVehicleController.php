3b393312b7887d61604166cbc2e00a0d2b934cff
ahanram@vamosystems.in <ahanram@vamosystems.in>
2016-03-11 23:01:35 +0530

    changes

--- app/controllers/VdmVehicleController.php
+++ app/controllers/VdmVehicleController.php
@@ -1532,6 +1532,7 @@ public function migrationUpdate() {
         $regNo = isset($refDataJson1['regNo'])?$refDataJson1['regNo']:'XXXXX';
         $vehicleMake =  isset($refDataJson1['vehicleMake'])?$refDataJson1['vehicleMake']:' ';
         $vehicleType =  isset($refDataJson1['vehicleType'])?$refDataJson1['vehicleType']:' ';I
+        
         $oprName = isset($refDataJson1['oprName'])?$refDataJson1['oprName']:'airtel';
         $mobileNo = isset($refDataJson1['mobileNo'])?$refDataJson1['mobileNo']:'0123456789';
         $overSpeedLimit = isset($refDataJson1['overSpeedLimit'])?$refDataJson1['overSpeedLimit']:'60';
