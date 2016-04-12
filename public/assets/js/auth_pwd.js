d06d8a05f8e22021f05979dabd8eb349a4db828b
arun_muhesh <mailtoarunmuhesh@vamosystems.in>
2016-04-12 10:31:10 +0530

    chnages

---
+++ public/assets/js/auth_pwd.js
@@ -0,0 +1,50 @@
+app.controller('mainCtrl',function($scope){
+	
+
+
+	//loading start function
+	var startLoading		= function () {
+		$scope.statusUi  = '';
+		$('#status').show(); 
+		$('#preloader').show();
+	};
+
+	//loading stop function
+	var stopLoading		= function () {
+		$('#status').fadeOut(); 
+		$('#preloader').delay(350).fadeOut('slow');
+		$('body').delay(350).css({'overflow':'visible'});
+	};
+
+	stopLoading();
+
+
+	// checking auth in the addsite and rfid 
+
+  	$scope.passwordConfirm  = function(){
+  		startLoading();
+  		
+   		var password  = $scope.pwd;
+    	var URL_ROOT    = "AddSiteController/";    /* Your website root URL */
+    	$.post(URL_ROOT+'checkPwd',{
+      		'_token': $('meta[name=csrf-token]').attr('content'),
+      		'pwd': password,
+    	}).done(function(data){
+      		console.log('Sucess---------->' + data+location.pathname);
+      		stopLoading();
+      		if(data == 'incorrect')
+      			$scope.statusUi   = "Invalid Password"; 
+      		else if (data == 'correct'){
+      			location.pathname = '/vamo/public/sites';
+      			sessionStorage.setItem('auth', 'sitesVal');
+      		}
+      			
+      			     
+    	}).fail(function(){
+    		stopLoading();
+      		console.log('fail');
+      		$scope.statusUi   = "Response Failure";  
+    	})
+	}
+
+});
