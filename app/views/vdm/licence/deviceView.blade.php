b8818decce16586b797c1ded842e036b7c96ce23
arun_muhesh <mailtoarunmuhesh@vamosystems.in>
2016-05-18 16:37:24 +0530

    changes

--- app/views/vdm/licence/deviceView.blade.php
+++ app/views/vdm/licence/deviceView.blade.php
@@ -1,11 +1,17 @@
+@extends('includes.vdmEditHeader')
+@section('mainContent')
+<head>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.11/css/dataTables.bootstrap.min.css">
 
+</head>
 <div id="wrapper">
 	<div class="content animate-panel">
 		<div class="row">
 			<div class="col-lg-12">
 				<div class="hpanel">
-					<div class="panel-heading">
-						<h4><font><b> View List </b> </font></h4>
+					<div class="panel-heading" align="center">
+						<h4><font><b> Licence View  </b> </font></h4>
 					</div>
 					<div class="panel-body">
 						
@@ -21,7 +27,8 @@
 							<div class="row">
 								<div class="col-md-12">
 									<hr>
-									<table id="example1" class="table table-bordered dataTable">
+                  
+									<table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
 										<thead>
 											<tr>
 											@if($i=='1')
@@ -43,17 +50,17 @@
 											<tr style="text-align: center;">
 											@if($i=='1')<td>{{ Form::checkbox('vehicleList[]', $val->vehicle_id , null, ['class' => 'field']) }}</td>@endif 
 												<td>{{ ++$key }}</td>
-												<td>{{ $val->vehicle_id }}</td>
+												<td class="vehiclelist">{{ $val->vehicle_id }}</td>
 												<td>{{ $val->device_id  }}</td>
 												<td>{{ $val->belongs_to  }}</td>
 												<td>{{ $val->status }}</td>
 												<td>{{ $val->sold_date }}</td>
 												<td>{{ $val->renewal_date }}</td>
-												<td>
+												<!-- <td>
 													
 													
 													
-												</td>
+												</td> -->
 
 											</tr>
 											@endforeach
@@ -70,9 +77,9 @@
 						</div>
 						@if($i=='1')
 						{{ Form::hidden('tempVal', $valueT) }}
-{{ Form::submit('renewal', array('class' => 'btn btn-sm btn-info')) }}
+            {{ Form::submit('renewal', array('class' => 'btn btn-sm btn-info')) }}
 
-@endif 
+            @endif 
 
 					</div>
 				</div>
@@ -80,195 +87,24 @@
 		</div>
 	</div>
 </div>
-<script src="../../vendor/jquery/dist/jquery.min.js"></script>
-<script src="../../vendor/jquery-ui/jquery-ui.min.js"></script>
-<script src="../../vendor/slimScroll/jquery.slimscroll.min.js"></script>
-<script src="../../vendor/bootstrap/dist/js/bootstrap.min.js"></script>
-<script src="../../vendor/jquery-flot/jquery.flot.js"></script>
-<script src="../../vendor/jquery-flot/jquery.flot.resize.js"></script>
-<script src="../../vendor/jquery-flot/jquery.flot.pie.js"></script>
-<script src="../../vendor/flot.curvedlines/curvedLines.js"></script>
-<script src="../../vendor/jquery.flot.spline/index.js"></script>
-<script src="../../vendor/metisMenu/dist/metisMenu.min.js"></script>
-<script src="../../vendor/iCheck/icheck.min.js"></script>
-<script src="../../vendor/peity/jquery.peity.min.js"></script>
-<script src="../../vendor/sparkline/index.js"></script>
-<script src="../../plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
-<script src="../../plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script> 
-<script src="../../vendor/validation/js/jquery.guardian-1.0.min.js"></script>
-<script src="../../scripts/homer.js"></script>
-<script src="../../scripts/charts.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.11/js/dataTables.bootstrap.min.js
+"></script>
 
 <script type="text/javascript">
+  
 
-
-                  
-                  
-                  
-                  $(function () {
-                      $('.check').on('click', function () {
-                        var valu = $('.check').each(function(){});
-                        var count = 0;
-                        var counter = 0;
-                        if(list.length)
-                        {
-                          for (var a in list){
-                          if(valu[0].checked == true)
-                            $(questionCheckBox[list[a]]).each(function(){ this.checked = true; });
-                          else if (valu[0].checked != true)
-                            $(questionCheckBox[list[a]]).each(function(){ this.checked = false; });
-                        };
-                          
-                      }
-                      else
-                      {
-                        // if($(".searchkey").t)
-                        if($( ".searchkey" ).val())
-                          for (var a in value){
-                              $(questionCheckBox[counter]).each(function(){ this.checked = false; });
-                            counter++;
-                          }
-                        else
-                          for (var a in value){
-                            if(valu[0].checked == true)
-                              $(questionCheckBox[count]).each(function(){ this.checked = true; });
-                            else if (valu[0].checked != true)
-                              $(questionCheckBox[count]).each(function(){ this.checked = false; });
-                            count++;
-                          };
-                        }
-                     }); 
-                  });
-                
-
-
-
-$( ".searchkey" ).keyup(function() {
-  list = [];
-  var valThis = $(this).val().toLowerCase();
-  $('.vehiclelist>input').each(function(index){
-    var text = $(this).val().toLowerCase();
-    if(text.indexOf(valThis) >= 0) {
-      $(this).parent('div').fadeIn();
-      list.push(index);
-    }
-    else{
-      $(this).parent('div').fadeOut();
-    }
-  });
-})
-
-
-      $(function () {
-        $("#example1").dataTable();
-
-       /* var table = $('#example1').DataTable();   userplace
-        var test  =   [];
-        $('#example1 tbody').on('click', 'td', function () {
-            $(this).closest('td').find("input").each(function() {
-                test.push(this.value);
-            });
-        });
-
-        $("form").submit(function(e)
-        {
-            vehicleList = test;
-            var formURL = $(this).attr("action");
-            $.ajax(
-            {
-                url : formURL,
-                type: "POST",
-                data : vehicleList,
-                success:function(data, textStatus, jqXHR) 
-                {
-                    //data: return data from server
-                },
-                error: function(jqXHR, textStatus, errorThrown) 
-                {
-                    //if fails      
-                }
-            });
-            e.preventDefault(); //STOP default action
-            //e.unbind(); //unbind. to stop multiple form submit.
-        });*/
-        
-      });
+$(document).ready(function() {
+    $('#example').DataTable();
+} );
 
 
 
 
-
-
-
-
-    </script>
-	
-
-<script>
-
-    $(function () {
-
-        /**
-         * Flot charts data and options
-         */
-        var data1 = [ [0, 55], [1, 48], [2, 40], [3, 36], [4, 40], [5, 60], [6, 50], [7, 51] ];
-        var data2 = [ [0, 56], [1, 49], [2, 41], [3, 38], [4, 46], [5, 67], [6, 57], [7, 59] ];
-
-        var chartUsersOptions = {
-            series: {
-                splines: {
-                    show: true,
-                    tension: 0.4,
-                    lineWidth: 1,
-                    fill: 0.4
-                },
-            },
-            grid: {
-                tickColor: "#f0f0f0",
-                borderWidth: 1,
-                borderColor: 'f0f0f0',
-                color: '#6a6c6f'
-            },
-            colors: [ "#62cb31", "#efefef"],
-        };
-
-        $.plot($("#flot-line-chart"), [data1, data2], chartUsersOptions);
-
-        /**
-         * Flot charts 2 data and options
-         */
-        var chartIncomeData = [
-            {
-                label: "line",
-                data: [ [1, 10], [2, 26], [3, 16], [4, 36], [5, 32], [6, 51] ]
-            }
-        ];
-
-        var chartIncomeOptions = {
-            series: {
-                lines: {
-                    show: true,
-                    lineWidth: 0,
-                    fill: true,
-                    fillColor: "#64cc34"
-
-                }
-            },
-            colors: ["#62cb31"],
-            grid: {
-                show: false
-            },
-            legend: {
-                show: false
-            }
-        };
-
-        $.plot($("#flot-income-chart"), chartIncomeData, chartIncomeOptions);
-
-
-
-    });
-
-</script> 
+</script>
+<div style="top: 0; left: 0;right: 0; padding-top: 30px;" align="center"><hr>@stop</div>
 </body>
 </html>
--- app/views/vdm/licence/licenceParent.blade.php
+++ app/views/vdm/licence/licenceParent.blade.php
@@ -1,75 +1,42 @@
 <div id="wrapper">
 	<div class="content animate-panel">
-		<div class="row">
-    		<div class="col-lg-12">
-       			 <div class="hpanel">
-       			 <h6><b><font color="red"> {{ HTML::ul($errors->all()) }}</font></b></h6>
-               		 <div class="panel-heading">
-                   		 <h4><b><font>Checkes licences</font></b></h4>
-                	 </div>
-                	<div class="panel-body">
-					<h4><font color="#196481">
-					</font></h4>
-					</br>
-					<br>
-                		<div id="example2_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer">
-                			<div class="row">
-                				<div class="col-sm-12">
-			                		
-									{{ Form::open(array('url' => 'Licence')) }}
-									<div class="row">
-										<div class="col-md-12">
-											<div class="col-md-8">
-												<div class="row">
-
-													<div class="col-md-6">
-														{{ Form::label('month', 'Month:') }}
-														{{ Form::select('month', array($month),$monthT , array('class' => 'form-control')) }}
-
-													</div>
-													<div class="col-md-6">
-														{{ Form::label('Year', 'year:') }}
-														{{ Form::select('year', array($year),$yearT , array('class' => 'form-control')) }}
-
-													</div>
-													<br/>
-													<div class="col-md-6">
-														{{ Form::label('Licence', 'Licence:') }}
-														{{ Form::select('Licence', array($Licence), $typeT, array('class' => 'form-control')) }}
-
-													</div>
-													<!-- <div class="col-md-6">
-														{{ Form::label('Payment_Mode', 'Payment_Mode:') }}
-														{{ Form::select('Payment_Mode', array($Payment_Mode), 'Monthly', array('class' => 'form-control')) }}
-
-													</div> -->
-													<div class="col-md-6">
-														{{ Form::label('own', 'Ownership:') }}
-														{{ Form::select('own', array($own), $ownT, array('class' => 'form-control')) }}
-
-													</div>
-													
-													<div class="col-md-6">
-		
-													</div>
-												</div>
-											</div>
-											<div class="col-md-6">
-											<div class="row">
-												<div class="col-md-6">	
-													</div>
-												<div class="col-md-6">
-													{{ Form::submit('Submit', array('class' => 'btn btn-primary')) }}{{ Form::close() }}
-												</div>	
-											</div>
-										</div>
-								</div>
-                			</div>
+		<div class="hpanel">
+       		<h6><b><font color="red"> {{ HTML::ul($errors->all()) }}</font></b></h6>
+           	<div class="panel-heading">
+             	<h4><b><font>Licences</font></b></h4>
+            </div>
+            <div class="panel-body">
+            	{{ Form::open(array('url' => 'Licence')) }}
+            	<div class="row">
+            		<div class="col-md-6">
+            			{{ Form::label('month', 'Month:') }}
+						{{ Form::select('month', array($month),$monthT , array('class' => 'form-control')) }}
             		</div>
-        		</div>
-    	  </div>
+            		<div class="col-md-6">
+            			{{ Form::label('Year', 'year:') }}
+						{{ Form::select('year', array($year),$yearT , array('class' => 'form-control')) }}
+            		</div>
+            	</div>
+            	<br>
+            	<div class="row">
+            		<div class="col-md-6">
+            			{{ Form::label('Licence', 'Licence:') }}
+						{{ Form::select('Licence', array($Licence), $typeT, array('class' => 'form-control')) }}
+            		</div>
+            		<div class="col-md-6">
+            			{{ Form::label('own', 'Ownership:') }}
+						{{ Form::select('own', array($own), $ownT, array('class' => 'form-control')) }}
+            		</div>
+            	</div>
+            	<br>
+            	<div class="row">
+            		<div class="col-md-9"></div>
+            		<div class="col-md-3">{{ Form::submit('Submit', array('class' => 'btn btn-primary')) }}</div>
+            	</div>
+            	{{ Form::close() }}
+            </div>
+		</div>
 	</div>
 </div>
 
-</div>
-</div>
+ 
