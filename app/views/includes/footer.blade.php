4d20422986f3c437bc709529cc8ea5922a2c49af
arun_muhesh <mailtoarunmuhesh@vamosystems.in>
2017-02-23 17:34:51 +0530

    copy rights problem

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,1 +1,2 @@
-<div id="copyright text-right" align="center"> © Copyright 2014-2016 GPSVTS</div>
+<!-- <div id="copyright text-right" align="center"> © Copyright 2014-2016 GPSVTS</div> -->
+<div  id="copyright text-right" align="center"></div>
