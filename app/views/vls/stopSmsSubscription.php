3769946146239d4db6a7b06cf446fb78a2e41fc4
arun_muhesh <mailtoarunmuhesh@vamosystems.in>
2017-03-17 01:18:56 +0530

    changes

--- app/views/vls/stopSmsSubscription.php
+++ app/views/vls/stopSmsSubscription.php
@@ -9,11 +9,11 @@ if (! Auth::check ()) {
 }
 
 $username 		= Auth::user ()->username;
-$vehiId       	= Input::get('vehicleId');
+$vehiId       	= Input::get('orgId');
 $mobileNo       = Input::get('mobNum');
-$stuDetails   	= Input::get('studentDetails');
 
-$data = array('vehicleId' => $vehiId, 'mobileNo' => $mobileNo, 'userId'=>$username);
+
+$data = array('orgId' => $vehiId, 'mobileNo' => $mobileNo, 'userId'=>$username);
 
 $url = 'http://' .$ipaddress . ':9000/stopSmsSubscription';
 log::info( ' Routing to backed :' . $url);
@@ -24,7 +24,9 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_TIMEOUT, 30);
 $response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
+log::info( 'curl status  :' .$httpCode  );
 echo $response;
 ?>
 
