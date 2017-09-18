9915d08f2c50e19cc1bc536d227d4c8a206adc95
Ramsrkn <ramakrishnan.vamosys@gmail.com>
2017-09-18 17:47:21 +0530

    dealer search to add edit report button

--- app/views/vdm/dealers/index1.blade.php
+++ app/views/vdm/dealers/index1.blade.php
@@ -92,7 +92,8 @@ tr:nth-of-type(odd) {
                     {{ Form::submit('Delete this Dealer', array('class' => 'btn btn-warning')) }}
                   {{ Form::close() }}
                   <a class="btn btn-small btn-success" href="{{ URL::to('vdmDealers/' . $value) }}">Show Dealer</a>
-                  <a class="btn btn-small btn-info" href="{{ URL::to('vdmDealers/' . $value . '/edit') }}">Edit Dealer</a>          
+                  <a class="btn btn-small btn-info" href="{{ URL::to('vdmDealers/' . $value . '/edit') }}">Edit Dealer</a> 
+                  <a class="btn btn-small btn-success" href="{{ URL::to('vdmUsers/reports/' . $value) }}">Edit Dealer Reports</a>				  
                 </td>
               </tr>
             @endforeach
