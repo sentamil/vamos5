fcc9560e00551550ce18720c8fb8c8338243d346
Ramsrkn <ramakrishnan.vamosys@gmail.com>
2017-10-25 18:13:58 +0530

    route cast honda9999

--- app/routes.php
+++ app/routes.php
@@ -942,7 +942,7 @@ Route::get('aUthName', array('uses' => 'HomeController@authName'));
 Route::get('logout', array('uses' => 'HomeController@doLogout'));
 Route::get('vdmVehicles/logout', array('uses' => 'HomeController@doLogout'));
  
-Route::get('yammaha8689', array('uses' => 'HomeController@admin'));
+Route::get('honda9999', array('uses' => 'HomeController@admin'));
 Route::get('adhocMail', array('uses' => 'HomeController@adhocMail'));
  
 Route::post('sendAdhocMail', array('before' => 'csrf', 'uses' => 'HomeController@sendAdhocMail'));
