2e39987ce8bde37a4fb450cf527ff6c72e9b384a
Ramsrkn <ramakrishnan.vamosys@gmail.com>
2018-02-15 15:24:07 +0530

    time search

--- app/views/includes/adminheader.blade.php
+++ app/views/includes/adminheader.blade.php
@@ -5,6 +5,11 @@
 <head>
 	<title>VAMO Systems</title>
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+	<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/css/bootstrap-select.min.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.6.3/js/bootstrap-select.min.js"></script>
 	
 </head>
 <body>
